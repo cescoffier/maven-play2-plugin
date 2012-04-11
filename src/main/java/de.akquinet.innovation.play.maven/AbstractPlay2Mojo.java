151258215744cf8aca49ab27964c04690085a0c1
Clement Escoffier <clement.escoffier@gmail.com>
2012-04-11 21:53:23 +0200

    #8 - Easier/more flexible PLAY2_HOME configuration
    Applied patch from Robin Palotai
    Allows the plugin to get the play2 home folder from a configuration settings or from an environmental property. This is used only if the PLAY2_HOME system property is not used.

--- src/main/java/de.akquinet.innovation.play.maven/AbstractPlay2Mojo.java
+++ src/main/java/de.akquinet.innovation.play.maven/AbstractPlay2Mojo.java
@@ -54,26 +54,51 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
      */
     MavenProjectHelper projectHelper;
 
+    /**
+     * The PLAY2_HOME path is taken from this setting, if not found as a Java system property (-DPLAY2_HOME).
+     * Refers to the PLAY2_HOME environment variable by default.
+     * <p/>
+     * So that means that the PLAY2_HOME can be given using:
+     * <ol>
+     * <li>A system variable defined by the system or with <tt>-DPLAY2_HOME=...</tt></li>
+     * <li>The <tt>play2Home</tt> configuration property</li>
+     * <li>The PLAY2_HOME environment property</li>
+     * </ol>
+     *
+     * @parameter expression="${env.PLAY2_HOME}"
+     */
+    String play2Home;
+
 
     public static final String ENV_PLAY2_HOME = "PLAY2_HOME";
 
+    /**
+     * Gets the specified <tt>PLAY2_HOME</tt> location.
+     * This method checks in this order:
+     * <ul>
+     *     <li>the PLAY2_HOME system variable</li>
+     *     <li>the <tt>play2Home</tt> settings or environment variable</li>
+     * </ul>
+     * If none is set, this method throws an exception.
+     * @return the play2 location
+     * @throws MojoExecutionException if the play2 location is not defined.
+     */
     public String getPlay2HomeOrThrow() throws MojoExecutionException {
         // First check, system variable        
-        String home = System.getProperty(ENV_PLAY2_HOME); 
-        if (home != null  && home.length() != 0) {
+        String home = System.getProperty(ENV_PLAY2_HOME);
+        if (home != null && !home.isEmpty()) {
             return home;
         }
 
-        // Second check, environment variable
-        home = System.getenv(ENV_PLAY2_HOME);
-        if (home != null  && home.length() != 0) {
-            return home;
+        // Second check, configuration or environment variable
+        if (play2Home != null && !play2Home.isEmpty()) {
+            return play2Home;
         }
 
-        throw new MojoExecutionException(ENV_PLAY2_HOME + " system variable not set");
+        throw new MojoExecutionException(ENV_PLAY2_HOME + " system/configuration/environment variable not set");
 
     }
-    
+
     public File getPlay2() throws MojoExecutionException {
         File play2 = null;
         String path = getPlay2HomeOrThrow();
@@ -83,7 +108,7 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
             play2 = new File(path, "play");
         }
 
-        if (! play2.exists()) {
+        if (!play2.exists()) {
             throw new MojoExecutionException("Can't find the play executable in " + path);
         }
 
@@ -93,7 +118,8 @@ public abstract class AbstractPlay2Mojo extends AbstractMojo {
     /**
      * Checks whether the current operating system is Windows.
      * This check use the <tt>os.name</tt> system property.
-      * @return <code>true</code> if the os is windows, <code>false</code> otherwise.
+     *
+     * @return <code>true</code> if the os is windows, <code>false</code> otherwise.
      */
     public boolean isWindows() {
         return System.getProperty("os.name").toLowerCase().indexOf("win") != -1;

4339a605e638d44c3d96341417747bfb62d21743
Clement Escoffier <clement.escoffier@gmail.com>
2013-02-24 11:22:54 +0100

    Add integration-tests using the invoker approach.
    Be careful, the build time is really slow….
    
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

---
+++ src/it/quickstart-scala/verify.groovy
@@ -0,0 +1,19 @@
+import java.io.*;
+
+def artifactNameWithoutExtension = "target/quickstart-scala-1.0-SNAPSHOT"
+
+// check the main jar artifact
+def jar = new File (basedir, artifactNameWithoutExtension + ".jar");
+assert jar.exists();
+assert jar.canRead();
+
+// check the zip distribution
+def dist = new File (basedir, artifactNameWithoutExtension + ".zip");
+assert dist.exists();
+assert dist.canRead();
+
+// check the war file
+def war = new File (basedir, artifactNameWithoutExtension + ".war");
+assert war.exists();
+assert war.canRead();
+

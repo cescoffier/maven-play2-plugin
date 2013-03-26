6499eb880c96b1be59f599ecf25641191a1ac9a9
Clement Escoffier <clement.escoffier@gmail.com>
2013-03-26 20:43:03 +0100

    Fix issue #25
    Provide an integration test to demonstrate #25
    (https://github.com/cescoffier/maven-play2-plugin/issues/25)
    
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

---
+++ src/it/maven-structure/project/Build.scala
@@ -0,0 +1,48 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  def fromEnv(name: String) = System.getenv(name) match {
+    case null => None
+    case value => Some(value)
+  }
+
+  val appName = fromEnv("project.artifactId").getOrElse("my-app")
+  val appVersion = fromEnv("project.version").getOrElse("1.0-SNAPSHOT")
+
+  System.out.println("AppName => " + appName + " / " + fromEnv("project.artifactId"))
+
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+  )
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Target folder
+    target <<= baseDirectory / "target",
+
+    // Source folders
+    sourceDirectory in Compile <<= baseDirectory / "src/main/java",
+    sourceDirectory in Test <<= baseDirectory / "src/test/java",
+
+    confDirectory <<= baseDirectory / "src/main/conf",
+    resourceDirectory in Compile <<= baseDirectory / "src/main/conf",
+
+    scalaSource in Compile <<= baseDirectory / "src/main/scala",
+    scalaSource in Test <<= baseDirectory / "src/test/scala",
+
+    javaSource in Compile <<= baseDirectory / "src/main/java",
+    javaSource in Test <<= baseDirectory / "src/test/java",
+
+
+    distDirectory <<= baseDirectory / "target/dist",
+
+    playAssetsDirectories := Seq.empty[File],
+
+    // The route file also needs to be updated...
+    playAssetsDirectories <+= baseDirectory / "src/main/resources"
+  )
+
+}
